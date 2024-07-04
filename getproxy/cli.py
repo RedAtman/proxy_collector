e9a5893539e58f79c55e31e381e31a9cc018ff69
redatman <evicwork@gmail.com>
2024-07-05 02:53:30 +0800

    style: formating cli.py

--- getproxy/cli.py
+++ getproxy/cli.py
@@ -5,8 +5,8 @@ from getproxy import GetProxy
 
 
 @click.command()
-@click.option('--in-proxy', help='Input proxy file')
-@click.option('--out-proxy', help='Output proxy file')
+@click.option("--in-proxy", help="Input proxy file")
+@click.option("--out-proxy", help="Output proxy file")
 def main(in_proxy, out_proxy):
     g = GetProxy(in_proxy, out_proxy)
     g.start()
